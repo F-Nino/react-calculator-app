1dfed0d5c5e3bfd28be062ca7ee8e173ff92b31b
Felipe Nino <felipe@Felipes-MacBook-Pro.local>
2019-06-27 11:12:08 -0500

    Updates

---
+++ precache-manifest.31957e91462e35aa97a4372e1213ef5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2837dc18902c0664b2ece608e9b72181",
+    "url": "/react-calculator-app/index.html"
+  },
+  {
+    "revision": "70c6eae15e464e20e04e",
+    "url": "/react-calculator-app/static/css/main.f14f76da.chunk.css"
+  },
+  {
+    "revision": "478ec4e9debab1903691",
+    "url": "/react-calculator-app/static/js/2.b893ebb9.chunk.js"
+  },
+  {
+    "revision": "70c6eae15e464e20e04e",
+    "url": "/react-calculator-app/static/js/main.875e749a.chunk.js"
+  },
+  {
+    "revision": "75c300b67db2a3f022b0",
+    "url": "/react-calculator-app/static/js/runtime~main.81ce0e12.js"
+  }
+]);
